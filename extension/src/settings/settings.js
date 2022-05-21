d3a78af69a65201934645d0f8d50eb72387cbe10
David <kdavid03@gmx.de>
2022-05-21 18:57:18 +0200

    settings page dev

--- extension/src/settings/settings.js
+++ extension/src/settings/settings.js
@@ -2,14 +2,25 @@
 // Static HTML elements
 const urlInput = document.getElementById("url-input");
 const saveSettingsButton = document.getElementById("save-settings-btn");
+// Get the current bookmarks API URL from local storage.
+async function getBookmarksUrl() {
+    const bookmarksApiUrl = await chrome.storage.local.get("bookmarks_api_url");
+    return bookmarksApiUrl.bookmarks_api_url || "";
+}
 // Save the URL to local storage.
-function saveBookmark() {
+function setBookmarksUrl() {
     chrome.storage.local.set({ "bookmarks_api_url": urlInput.value });
+    saveSettingsButton.disabled = true;
+}
+// Enable the save button if the URL input has changed.
+async function onUrlInputChange() {
+    saveSettingsButton.disabled = await getBookmarksUrl() === urlInput.value;
 }
 // Set the URL input to the cached value.
 async function init() {
-    const bookmarksApiUrl = await chrome.storage.local.get("bookmarks_api_url");
-    urlInput.value = bookmarksApiUrl.bookmarks_api_url || "";
-    saveSettingsButton.onclick = saveBookmark;
+    urlInput.value = await getBookmarksUrl();
+    saveSettingsButton.onclick = setBookmarksUrl;
+    urlInput.onchange = setBookmarksUrl;
+    urlInput.oninput = onUrlInputChange;
 }
 window.onload = init;
--- extension/ts_settings/settings.ts
+++ extension/ts_settings/settings.ts
@@ -2,17 +2,30 @@
 const urlInput = document.getElementById("url-input") as HTMLInputElement;
 const saveSettingsButton = document.getElementById("save-settings-btn") as HTMLButtonElement;
 
+// Get the current bookmarks API URL from local storage.
+async function getBookmarksUrl(): Promise<string> {
+  const bookmarksApiUrl = await chrome.storage.local.get("bookmarks_api_url");
+  return bookmarksApiUrl.bookmarks_api_url || "";
+}
+
 // Save the URL to local storage.
-function saveBookmark(): void {
-    chrome.storage.local.set({ "bookmarks_api_url": urlInput.value });
+function setBookmarksUrl(): void {
+  chrome.storage.local.set({ "bookmarks_api_url": urlInput.value });
+  saveSettingsButton.disabled = true;
+}
+
+// Enable the save button if the URL input has changed.
+async function onUrlInputChange(): Promise<void> {
+  saveSettingsButton.disabled = await getBookmarksUrl() === urlInput.value;
 }
 
 // Set the URL input to the cached value.
 async function init(): Promise<void> {
-    const bookmarksApiUrl = await chrome.storage.local.get("bookmarks_api_url");
-    urlInput.value = bookmarksApiUrl.bookmarks_api_url || "";
+  urlInput.value = await getBookmarksUrl();
 
-    saveSettingsButton.onclick = saveBookmark;
+  saveSettingsButton.onclick = setBookmarksUrl;
+  urlInput.onchange = setBookmarksUrl;
+  urlInput.oninput = onUrlInputChange;
 }
 
 window.onload = init;
