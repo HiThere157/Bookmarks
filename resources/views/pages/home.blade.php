9794a744f404aaee2abe27fe08b1632d34756000
David <kdavid03@gmx.de>
2022-06-01 17:02:38 +0200

    css fix

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,7 +8,7 @@
     <div class="container-fluid d-flex flex-wrap">
         @foreach($bookmarks as $bookmark)
             <a class="d-flex justify-content-center align-items-center flex-column rounded shadow m-3 text-decoration-none" 
-                style="width: 10%; min-width: 10rem; aspect-ratio: 1" 
+                style="width: 15%; min-width: 15rem; aspect-ratio: 1.7" 
                 href="{{ $bookmark->url }}">
                 <h1>
                     {{ $bookmark->title }}
